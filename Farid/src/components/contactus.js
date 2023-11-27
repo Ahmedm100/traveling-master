e5fd2e80a2259088ffe6ee3f8dce7e5e593b0a13
KosBow <kosrat_95@hotmail.com>
2023-11-28 00:32:22 +0100

    lite buf fix på contactus.js

--- Farid/src/components/contactus.js
+++ Farid/src/components/contactus.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "../components/contactus.css";
-import Image from "../assets/contact_image.jpg"
+import "../components/Styles/contactus.css";
+import Image from "../assets/contact_image.jpg";
 
 
 
