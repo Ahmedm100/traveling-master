546b55fc59b084ee3d03a0c5f8ff71b74dea46ac
HajwaaN <kevin-hattina@hotmail.com>
2023-11-27 22:13:26 +0100

    contact-branch: lagt till kontakt sida  och modiferat kod

--- Farid/src/App.js
+++ Farid/src/App.js
@@ -3,7 +3,7 @@ import { Routes,Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Pricing from "./routes/Pricing";
 import Training from './routes/Training'
-import Contact from "./routes/Contact";
+import Contactus from "./routes/Contact";
 import Bookning from "./routes/Bookning";
 
 
@@ -14,7 +14,7 @@ function App() {
       <Route path='/'element={<Home />} />
       <Route path="/pricing" element={<Pricing />} />
       <Route path="/training" element={<Training />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/contact" element={<Contactus />} />
       <Route path="/bookning" element={<Bookning />} />
       </Routes>
     </>
---
+++ Farid/src/components/contactus.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../components/contactus.css";
+import Image from "../assets/contact_image.jpg"
+
+
+
+
+function Contactus() {
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+      ></div>
+
+        <img src={Image} alt="image" className="image"></img>
+
+
+      <div className="rightSide">
+        <h1> Contact Us</h1>
+
+
+        <form id="contact-form" method="POST">
+          <label htmlFor="name">Full Name</label>
+          <input name="name" placeholder="Enter full name..." type="text" />
+          <label htmlFor="email">Email</label>
+          <input name="email" placeholder="Enter email..." type="email" />
+          <label htmlFor="message">Message</label>
+          <textarea
+            rows="6"
+            placeholder="Enter message..."
+            name="message"
+            required
+          ></textarea>
+          <button type="submit"> Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contactus;
