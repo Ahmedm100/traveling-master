e19da341f17c486887e8b5a62b54012db0db3ab7
KosBow <kosrat_95@hotmail.com>
2023-11-30 04:08:21 +0100

    fixing error

--- Farid/src/routes/Contact.js
+++ Farid/src/routes/Contact.js
@@ -1,29 +1,16 @@
 import React from "react";
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import Contactus from "../components/contactus";
 
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <Contactus />
+      <Footer />
+    </div>
+  );
+};
 
-
-
-const Contact =()=>{
-
-return(
-
-<div>
-
-  <Navbar />
-  <Contactus />
-  <Footer /> 
-
-</div>
-
-
-
-)
-
-
-
-}
-
-export default Contact
+export default Contact;
--- Farid/src/routes/Pricing.js
+++ Farid/src/routes/Pricing.js
@@ -4,23 +4,15 @@ import Footer from "../components/Footer";
 import PricingCards from "../components/Pricing";
 import HeroImage from "../components/Heroimage";
 
-const Pricing =()=>{
-return(
+const Pricing = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroImage heading="PRICING." text="Choose your trip" />
+      <PricingCards />
+      <Footer />
+    </div>
+  );
+};
 
-<div>
-
-<Navbar />
-<HeroImage heading='PRICING.' text='Choose your trip'/>
-<PricingCards />
-<Footer />
-
-
-</div>
-
-
-)
-
-
-}
-
-export default Pricing
+export default Pricing;
